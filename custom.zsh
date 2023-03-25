b12cd0058394bd4775150c7e8b219988c4b0d052
Your Name <you@example.com>
2023-03-25 15:25:26 +0100

    Remove bash git branch function

--- custom.zsh
+++ custom.zsh
@@ -125,11 +125,6 @@ alias st='git status -b -s'
 
 alias lv='git log --graph --decorate'
 
-parse_git_branch() {
-     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-}
-export PS1="\u@\h \[\e[32m\]\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\]$ "
-
 
 ######################### C STUFF ######################
 
